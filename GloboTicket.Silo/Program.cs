9ab14ae492e7b0ff53f4e08022a4873510d68a67
monem.benjedou <monembenjedou@takiacademyteam.com>
2024-05-17 14:18:22 +0100

    Fixed stuff

--- GloboTicket.Silo/Program.cs
+++ GloboTicket.Silo/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Orleans.Configuration;
+using System.Net.Sockets;
+using System.Net;
 
 try
 {
@@ -19,24 +21,24 @@ catch (Exception ex)
     Console.WriteLine(ex);
     return 1;
 }
-
 static async Task<IHost> StartSiloAsync()
 {
-    var persistanceConnectionString = "Server=192.168.30.55,1435;Database=orleans;User Id=sa;Password=Monemnejijannet1;";
-
+    var persistanceConnectionString = "Server=192.168.30.55;Database=orleans;User Id=sa;Password=Monemnejijannet1;";
 
     var builder = Host
         .CreateDefaultBuilder()
         .UseOrleans((context, silo) =>
         {
-            silo.Configure<ClusterOptions>(options =>
-            {
-                options.ClusterId = "dev";
-                options.ServiceId = "GloboTicket";
-            })
+            silo
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "GloboTicket";
+                })
                 .UseAdoNetClustering(options =>
                 {
                     options.ConnectionString = persistanceConnectionString ?? " ";
+
                     options.Invariant = "System.Data.SqlClient";
                 }).AddAdoNetGrainStorage("profiles", options =>
                 {
@@ -46,21 +48,24 @@ static async Task<IHost> StartSiloAsync()
                 {
                     options.ConnectionString = persistanceConnectionString ?? " ";
                     options.Invariant = "System.Data.SqlClient";
-                })
-                .ConfigureLogging(logging => logging.AddConsole()).UseDashboard(options =>
-            {
-                options.Username = "username";
-                options.Password = "password";
-                options.Host = "localhost";
-                options.Port = 9000;
-                options.HostSelf = true;
-                options.CounterUpdateIntervalMs = 1000;
+                }).Configure<EndpointOptions>(options =>
+                {
+                    options.SiloPort = 11111; 
+                    options.GatewayPort = 30000;
+                }).ConfigureLogging(logging => logging.AddConsole()).UseDashboard(options =>
+                {
+                    options.Username = "username";
+                    options.Password = "password";
+                    options.Host = "localhost";
+                    options.Port = 9000;
+                    options.HostSelf = true;
+                    options.CounterUpdateIntervalMs = 1000;
             }); ;
 
         });
 
     var host = builder.Build();
-    await host.StartAsync();
+    await host.RunAsync();
 
     return host;
 }
